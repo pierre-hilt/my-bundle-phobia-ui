8a6238d77cc6eeeb9666fce86f2e424baccf5f77
philt <pierre.hilt@gmail.com>
2019-10-14 22:17:44 +0200

    feat(mock): add mocking capability

---
+++ src/app/api/size/size.mock.ts
@@ -0,0 +1,15 @@
+export const sizeMock = {
+  assets: [{ gzip: 2699, name: 'main', size: 6675, type: 'js' }],
+  dependencyCount: 3,
+  dependencySizes: [{ approximateSize: 6683, name: 'react' }],
+  description: 'React is a JavaScript library for building user interfaces.',
+  gzip: 2699,
+  hasJSModule: false,
+  hasJSNext: false,
+  hasSideEffects: true,
+  name: 'react',
+  repository: 'https://github.com/facebook/react.git',
+  scoped: false,
+  size: 6675,
+  version: '16.10.2'
+};
--- src/app/api/size/size.service.ts
+++ src/app/api/size/size.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { environment } from 'src/environments/environment.mock';
+import { sizeMock } from './size.mock';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +12,9 @@ export class SizeService {
   constructor(private httpClient: HttpClient) {}
 
   getSize(packageName: string): Observable<any> {
+    if (environment.mock) {
+      return of(sizeMock);
+    }
     return this.httpClient.get(`/${this.apiKey}?package=${packageName}`);
   }
 }
